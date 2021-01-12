f1862e0027b1de4263ffc1c1cd492d876f9da8ad
Alejandro Pérez <56156526+AlejandroPerezAnuncibay@users.noreply.github.com>
2021-01-12 09:45:52 +0100

    Update Database.php

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -3,7 +3,7 @@ class Database {
 	public static $db;
 	public static $con;
 	function Database(){
-		$this->user="b7da8cbe3ba44c";$this->pass="92ba4f62";$this->host="eu-cdbr-west-03.cleardb.net";$this->ddbb="bookmedik";
+		$this->user="b7da8cbe3ba44c";$this->pass="92ba4f62";$this->host="eu-cdbr-west-03.cleardb.net";$this->ddbb="heroku_753e5c1cd88e86e";
 	}
 
 	function connect(){
